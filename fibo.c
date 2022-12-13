bddb8e38127d2587203c9d0761a74012318b5997
TimSpakowski <119943071+TimSpakowski@users.noreply.github.com>
2022-12-13 15:13:11 +0100

    Update fibo.c

--- fibo.c
+++ fibo.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-// Funktion zum Berechnen der Fibonacci-Zahl fn
 int fibonacci(int n) {
   if (n == 0) return 0;
   if (n == 1) return 1;
@@ -8,18 +7,15 @@ int fibonacci(int n) {
 }
 
 int main() {
-  // Variablen für die Eingabe und Ausgabe
+  
   int n;
   int fn;
 
-  // Eingabeaufforderung und Eingabe
   printf("Gib eine Zahl n ein: ");
   scanf("%d", &n);
-
-  // Berechnung der Fibonacci-Zahl fn
+  
   fn = fibonacci(n);
 
-  // Ausgabe der Fibonacci-Zahl fn
   printf("Die Fibonacci-Zahl fn ist: %d\n", fn);
 
   return 0;
